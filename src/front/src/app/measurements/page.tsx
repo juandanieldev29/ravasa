871f045d1a013001bd7d49e5383a0d7d9530efeb
Juan Vargas <juandanielvargascordero@gmail.com>
2025-09-19 15:36:26 -0600

    adding loading spinner

--- src/front/src/app/measurements/page.tsx
+++ src/front/src/app/measurements/page.tsx
@@ -1,9 +1,15 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { fetchAuthSession, fetchUserAttributes, signOut } from '@aws-amplify/auth';
 import { Amplify, ResourcesConfig } from 'aws-amplify';
+
+import Spinner from '@/components/spinner';
+
+import LoadingContextProvider from '@/contexts/loading-context';
+import { LoadingContext } from '@/contexts/loading-context';
+import { LoadingAction } from '@/enums/loading-action';
 
 import '@aws-amplify/ui-react/styles.css';
 
@@ -32,21 +38,34 @@ Amplify.configure(config, { ssr: true });
 export default function Measurements() {
   const [givenName, setGivenName] = useState<null | string>(null);
   const [email, setEmail] = useState<null | string>(null);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const { dispatch } = useContext(LoadingContext);
+
   const fetchUserProfile = async () => {
-    const session = await fetchAuthSession();
-    if (!session.tokens?.idToken) {
-      console.log('There is no auth session');
-      signOut();
-      return;
+    try {
+      dispatch({ type: LoadingAction.INCREASE_HTTP_REQUEST_COUNT });
+      const session = await fetchAuthSession();
+      if (!session.tokens?.idToken) {
+        console.log('There is no auth session');
+        signOut();
+        return;
+      }
+      const userAttributes = await fetchUserAttributes();
+
+      if (!userAttributes['email'] || !userAttributes['given_name']) {
+        console.log('User does not have email or given name');
+        signOut();
+        return;
+      }
+      const isAdmin = userAttributes['custom:isAdmin'] === 'true' ? true : false;
+      setEmail(userAttributes['email']);
+      setGivenName(userAttributes['given_name']);
+      setIsAdmin(isAdmin);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      dispatch({ type: LoadingAction.DECREASE_HTTP_REQUEST_COUNT });
     }
-    const { email, given_name } = await fetchUserAttributes();
-    if (!email || !given_name) {
-      console.log('User does not have email or given name');
-      signOut();
-      return;
-    }
-    setEmail(email);
-    setGivenName(given_name);
   };
 
   useEffect(() => {
@@ -56,12 +75,16 @@ export default function Measurements() {
   return (
     <Authenticator.Provider>
       <Authenticator socialProviders={['google']} signUpAttributes={['email']}>
-        {givenName && (
-          <>
-            <p>Hola {givenName}</p>
-            <p>Tu correo es {email}</p>
-          </>
-        )}
+        <LoadingContextProvider>
+          <Spinner />
+          {givenName && (
+            <>
+              <p>Hola {givenName}</p>
+              <p>Tu correo es {email}</p>
+              <p>Eres administrador? {isAdmin ? 'Si' : 'No'}</p>
+            </>
+          )}
+        </LoadingContextProvider>
       </Authenticator>
     </Authenticator.Provider>
   );
---
+++ src/front/src/contexts/loading-context.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useReducer, createContext, ReactNode } from 'react';
+
+interface LoadingState {
+  httpRequestsCount: number;
+}
+
+type LoadingAction =
+  | { type: 'INCREASE_HTTP_REQUEST_COUNT' }
+  | { type: 'DECREASE_HTTP_REQUEST_COUNT' };
+
+const initialState: LoadingState = { httpRequestsCount: -1 };
+
+const loadingReducer = (state: LoadingState, action: LoadingAction): LoadingState => {
+  switch (action.type) {
+    case 'INCREASE_HTTP_REQUEST_COUNT':
+      return { ...state, httpRequestsCount: state.httpRequestsCount + 1 };
+    case 'DECREASE_HTTP_REQUEST_COUNT':
+      return { ...state, httpRequestsCount: state.httpRequestsCount - 1 };
+    default:
+      return state;
+  }
+};
+
+interface LoadingContextProps {
+  state: LoadingState;
+  dispatch: React.Dispatch<LoadingAction>;
+}
+
+export const LoadingContext = createContext<LoadingContextProps>(null!);
+
+export default function LoadingContextProvider({ children }: { children: ReactNode }) {
+  const [state, dispatch] = useReducer(loadingReducer, initialState);
+
+  return <LoadingContext.Provider value={{ state, dispatch }}>{children}</LoadingContext.Provider>;
+}
