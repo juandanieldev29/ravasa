a8c2d07a686f26f90e7de16ef3dd88d67e4ad598
Juan Vargas <juandanielvargascordero@gmail.com>
2025-09-23 13:50:53 -0600

    specifying local origin for testing purposes

--- src/front/src/app/measurements/page.tsx
+++ src/front/src/app/measurements/page.tsx
@@ -30,8 +30,8 @@ const config: ResourcesConfig = {
         oauth: {
           domain: process.env.NEXT_PUBLIC_USER_POOL_DOMAIN_URL!,
           scopes: ['openid', 'profile', 'email', 'aws.cognito.signin.user.admin'],
-          redirectSignIn: ['https://dev.d22jw8grtfyd4.amplifyapp.com/measurements'],
-          redirectSignOut: ['https://dev.d22jw8grtfyd4.amplifyapp.com/measurements'],
+          redirectSignIn: ['http://localhost:3000/measurements'],
+          redirectSignOut: ['http://localhost:3000/measurements'],
           responseType: 'code',
         },
       },
--- src/front/src/utils/amplifyServerUtils.ts
+++ src/front/src/utils/amplifyServerUtils.ts
@@ -11,8 +11,8 @@ export const { runWithAmplifyServerContext } = createServerRunner({
           oauth: {
             domain: process.env.USER_POOL_DOMAIN_URL!,
             scopes: ['openid', 'profile', 'email', 'aws.cognito.signin.user.admin'],
-            redirectSignIn: ['https://dev.d22jw8grtfyd4.amplifyapp.com/measurements'],
-            redirectSignOut: ['https://dev.d22jw8grtfyd4.amplifyapp.com/measurements'],
+            redirectSignIn: ['http://localhost:3000/measurements'],
+            redirectSignOut: ['http://localhost:3000/measurements'],
             responseType: 'code',
           },
         },
